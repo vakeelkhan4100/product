e96eef3c0a080156e1853783855ef4ca4ac1bc5a
Jamil khan <jamilkhan@Jamils-MacBook-Pro.local>
2022-11-01 10:30:09 +0530

    message

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import hostconnect from "./config/db.js";
 import {product_data} from "./routes/product.route.js";
 import { user } from "./routes/user.route.js";
 import { reviewrating } from "./routes/review.rating.route.js";
-
+import { config } from 'dotenv';
+config();
 const app = express();
 app.use(express.json());
 hostconnect();
@@ -14,6 +15,6 @@ app.use(sub_cate)
 app.use(product_data)
 app.use(user)
 // app.use(reviewrating)
-app.listen( process.env.PORT||3001, (req, res) => {
+app.listen(process.env.PORT||3001, (req, res) => {
     console.log("server is run on 2000 port");
 })
