f27e29be58c595abe2f5c9c4f381fd90e1d85640
Jamil khan <jamilkhan@Jamils-MacBook-Pro.local>
2022-11-02 21:38:41 +0530

    message

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 const hostconnect = async () => {
-    const conn = await mongoose.connect("mongodb://localhost:27017/deshwali")
+    const conn = await mongoose.connect("mongodb+srv://vakeel:vakeel@pixel.rjqqvvg.mongodb.net/?retryWrites=true&w=majority")
     console.log("host-----", conn.connection.host)
 }
 export default hostconnect;
